01b3f9eb39e876a2952fa73678e17bc91418bdf3
vavrines <tianbaixiao@gmail.com>
2025-10-31 16:55:02 +0800

    Add download script

---
+++ download.sh
@@ -0,0 +1,4 @@
+wget -P mnist/ https://storage.googleapis.com/cvdf-datasets/mnist/train-images-idx3-ubyte.gz
+wget -P mnist/ https://storage.googleapis.com/cvdf-datasets/mnist/train-labels-idx1-ubyte.gz
+wget -P mnist/ https://storage.googleapis.com/cvdf-datasets/mnist/t10k-images-idx3-ubyte.gz
+wget -P mnist/ https://storage.googleapis.com/cvdf-datasets/mnist/t10k-labels-idx1-ubyte.gz
